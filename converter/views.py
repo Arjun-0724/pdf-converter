22c901a66b15fe5dab2888ef951c712ccd42319d
arkr24 <you@example.com>
2026-06-15 16:34:41 +0530

    Add file upload form

--- converter/views.py
+++ converter/views.py
@@ -3,10 +3,18 @@ from .forms import WordUploadForm
 
 def home(request):
 
+    if request.method == "POST":
+
+        uploaded_file = request.FILES["document"]
+
+        print(uploaded_file.name)
+
     form = WordUploadForm()
 
     return render(
         request,
         "converter/home.html",
-        {"form": form}
+        {
+            "form": form
+        }
     )
