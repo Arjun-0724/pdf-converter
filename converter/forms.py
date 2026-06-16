fe59b1dfd63b4394d141b6a7fdd7b3193bd340b1
arkr24 <you@example.com>
2026-06-16 15:59:03 +0530

    Add unique filenames and automatic file cleanup

--- converter/forms.py
+++ converter/forms.py
@@ -1,18 +1,29 @@
 from django import forms
+from .services.file_service import *
 
 class WordUploadForm(forms.Form):
-
-    document = forms.FileField(
-        label="Select DOCX File"
-    )
+    document = forms.FileField()
 
     def clean_document(self):
+        file = self.cleaned_data["document"]
 
-        document = self.cleaned_data["document"]
+        allowed_extensions = [
+            ".docx"
+        ]
 
-        if not document.name.endswith(".docx"):
+        extension = (
+            "." +
+            file.name.split(".")[-1].lower()
+        )
+
+        if extension not in allowed_extensions:
             raise forms.ValidationError(
                 "Only DOCX files are allowed."
             )
 
-        return document
+        if file.size > 20 * 1024 * 1024:
+            raise forms.ValidationError(
+                "File size cannot exceed 20 MB."
+            )
+
+        return file
