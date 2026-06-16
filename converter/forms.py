de648a6c1c84f524976f5dd3df2fd3e197e2c16a
arkr24 <you@example.com>
2026-06-16 13:06:29 +0530

    Add DOCX to PDF conversion and download

--- converter/forms.py
+++ converter/forms.py
@@ -1,6 +1,18 @@
 from django import forms
 
 class WordUploadForm(forms.Form):
+
     document = forms.FileField(
         label="Select DOCX File"
     )
+
+    def clean_document(self):
+
+        document = self.cleaned_data["document"]
+
+        if not document.name.endswith(".docx"):
+            raise forms.ValidationError(
+                "Only DOCX files are allowed."
+            )
+
+        return document
